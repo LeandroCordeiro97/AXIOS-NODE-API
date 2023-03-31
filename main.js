0f4df7a309fd1801bd3157833d55dbc859ac8bfe
LeandroCordeiro97 <leandrocordeiro97@live.com.pt>
2023-03-31 13:16:40 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const url ="http://localhost:5500/api"
+
+function getUsers(){
+    axios.get(url)
+    .then(response =>{
+        apiResult.textContent =JSON.stringify(response.data)
+    })
+    .catch(error => console.error(error))
+}
+
+function addNewUser(newUser){
+    axios.post(url,newUser )
+    .then(response =>{
+        console.log(response)
+    })
+    .catch(error => console.error(error))
+}
+getUsers()
+
+const newUser={
+    name:"Leandro Cordeiro",
+    avatar:"https://picsum.photos/200/300",
+    city:"Portugal"
+}
+addNewUser()
